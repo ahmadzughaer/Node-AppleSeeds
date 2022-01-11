973e834ab81fc45dba507a38c6e978515f89ba54
ahmadzughaer <ahmadzughaer@gmail.com>
2022-01-11 16:35:42 +0200

    assignment 6.1

--- week-one/hello.mjs
+++ week-one/hello.mjs
@@ -1,1 +1,45 @@
-console.log('welcome to our app')
+// const square = function(x) {
+//     return x * x
+// }
+
+// const square = (x) => {
+//     return x * x
+// }
+
+// const square = x => x*x
+// console.log(square(2))
+
+// const myEvent = {
+//     name: "birthday party",
+//     guestList: ['ahmad', 'mike', 'mr bean', 'superman'],
+//     printguestList() {
+//         const that = this
+//         console.log('My guest list ' + this.name)
+//         this.guestList.forEach(function (guest) {
+//             console.log(guest + " is attending " + that.name)
+//         })
+//     }
+// }
+
+// myEvent.printguestList()
+// 1. Define getTasksToDo method
+// 2. Use filter to to return just the incompleted tasks (arrow function)
+// 3. Test your work by running the script
+
+const tasks = {
+    tasks: [{
+        text: 'Grocery shopping',
+        completed: true
+    }, {
+        text: 'Clean yard',
+        completed: false
+    }, {
+        text: 'Film course',
+        completed: false
+    }],
+    getTasksToDo() {
+       const tasksToDO = this.tasks.filter((task) => task.completed === false);
+       return tasksToDO
+    }
+}
+console.log(tasks.getTasksToDo())
